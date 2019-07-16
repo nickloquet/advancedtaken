d2a99de62c8650e362c13a9cf935fa363b29cab7
nick.loquet <nick_loquet@live.be>
2019-07-16 11:01:01 +0200

    Initial commit

---
+++ src/main/java/be/vdab/advancedtaken/AdvancedtakenApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.advancedtaken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedtakenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedtakenApplication.class, args);
+    }
+
+}
